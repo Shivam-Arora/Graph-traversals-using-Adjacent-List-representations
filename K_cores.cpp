c39747cc92dc09d191a2f7a21f62b570fd7fc7df
Divyanshu Gupta <divyanshuguptadg1997@gmail.com>
2018-06-13 18:52:04 +0530

    Update K_cores.cpp

--- K_cores.cpp
+++ K_cores.cpp
@@ -471,4 +471,7 @@ void getMaxCoins(int pos, int coins, int n, int &maxCoins){
 if (pos < 0 || pos > 4 || coins < 0) return;
 if (a[n - 1][pos] == 2) coins -= 1; else if (a[n - 1][pos] == 1) coins += 1;
 if (n == 1){ if (coins > maxCoins) maxCoins = coins; return; } else{ // 3 options // move right getMaxCoins(pos + 1, coins, n - 1,
-	*/
+	
+	
+	
+	We have a game where an airplane is placed in the middle column of the bottom row. The airplane can move right or left by one step and in every step the row moves down. When the airplane meets ‘1’ (coin) the number of points increase by 1 and when the airplane meets ‘2’ (bomb) the number of points decrease by 1. Whenever the airplane meets the bomb with score 0 the airplane dies and game is over. The user has one detonate option throughout the game where he can detonate all the bombs in the next 5 rows. Find the maximum number of points (coins) that can be collected by the user. Number of rows 1 <= N <= 12. Return -1 if score < 0*/
