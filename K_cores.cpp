24d8f8ee2da79b4dfcf9c4e100c7fa8940da4221
Divyanshu Gupta <divyanshuguptadg1997@gmail.com>
2018-06-13 11:50:13 +0530

    Update K_cores.cpp

--- K_cores.cpp
+++ K_cores.cpp
@@ -141,3 +141,156 @@ int main(){
  
     cout << endl << endl;	
 }
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+//  \  "\n"
+//   ||
+struct adjlistnode{
+    int data;
+    struct adjlistnode *next;
+};
+struct adjlist{
+    struct adjlistnode *head;
+};
+struct graph{
+    int v;
+    struct adjlist *arr;
+};
+struct qnode{
+    int data;
+    struct qnode *next;
+};
+struct queue{
+    struct qnode *front;
+    struct qnode *rear;
+};
+struct qnode * newqnode(int data){
+    struct qnode *temp=(struct qnode *)malloc(sizeof(struct qnode));
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
+struct queue *createqueue(){
+    struct queue* temp=(struct queue *)malloc(sizeof(struct queue));
+    temp->front=NULL;
+    temp->rear=NULL;
+    return temp;
+}
+void push_in_queue(struct queue *q,int data){
+    struct qnode *temp = newqnode(data);
+    if(q->rear==NULL){
+        q->front=q->rear=temp;
+        return;
+    }
+    q->rear->next=temp;
+    q->rear=temp;
+}
+struct qnode* pop_from_queue(struct queue *q){
+    if(q->front==NULL){
+        return NULL;
+    }
+    struct qnode *temp=q->front;
+    q->front=q->front->next;
+
+    if(q->front==NULL){
+        q->rear=NULL;
+    }
+    return temp;
+}
+int isEmpty(struct queue *q){
+    if(q->rear==NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+struct adjlistnode* new_adjlistnode(int data){
+    struct adjlistnode *temp=(struct adjlistnode*)malloc(sizeof(struct adjlistnode));
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
+struct graph* creategraph(int v){
+    struct graph* temp=(struct graph *)malloc(sizeof(struct graph));
+    temp->v=v;
+    temp->arr=(struct adjlist *)malloc(v*sizeof(struct adjlist));
+
+    int i;
+    for(i=0;i<v;i++){
+        temp->arr[i].head=NULL;
+    }
+    return temp;
+}
+void addedge(struct graph *g,int src,int dest){
+    struct adjlistnode *temp=new_adjlistnode(dest);
+    temp->next = g->arr[src].head;
+    g->arr[src].head=temp;
+
+    temp=new_adjlistnode(src);
+    temp->next=g->arr[dest].head;
+    g->arr[dest].head=temp;
+}
+void BFS(struct graph *g,int s){
+    int visited[g->v];
+    int i;
+    for(i=0;i<g->v;i++){
+        visited[i]=0;
+    }
+    visited[s]=1;
+
+    struct queue *q=createqueue();
+    push_in_queue(q,s);
+
+    while(isEmpty(q)==0){
+        struct qnode *f= q->front;
+        printf("%d\n",f->data);
+        pop_from_queue(q);
+
+        struct adjlistnode *t=g->arr[f->data].head;
+
+        while(t!=NULL){
+            if(visited[t->data]==0){
+                push_in_queue(q,t->data);
+                visited[t->data]=1;
+            }
+            t=t->next;
+        }
+    }
+}
+void printGraph(struct graph *g){
+    int i;
+    for(i=0;i<g->v;i++){
+        printf("\n Adjacency list : %d\n head ", i);
+        struct adjlistnode *head=g->arr[i].head;
+        while(head!=NULL){
+            printf("--> %d ",head->data);
+            head=head->next;
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int v=5;
+
+    struct graph *g=creategraph(v);
+    addedge(g,0,1);
+    addedge(g,0,4);
+    addedge(g,1,2);
+    addedge(g,1,3);
+    addedge(g,1,4);
+    addedge(g,2,3);
+    addedge(g,3,4);
+
+    printGraph(g);
+
+    printf("\n\n");
+
+    BFS(g,2);
+
+}
+//   "\n"
+
+*/
